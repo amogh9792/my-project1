7a7c930b4b732403671ac085c820ff01b1e83b97
Amogh Pathak <amogh9792@gmail.com>
2023-12-31 12:07:57 +0530

    project architecture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from source.entity.config_entity import TrainingPipelineConfig
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    train_pipeline_config_obj = TrainingPipelineConfig()
+
+    print(train_pipeline_config_obj.__dict__)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
